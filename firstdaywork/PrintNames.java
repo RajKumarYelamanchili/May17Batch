189681d75d4f58dfe9eb93f09c120e611b76ad7e
RajKumar <yrajkumar.vzg@gmail.com>
2025-05-27 16:23:12 +0530

    this is 2 files from Raj

---
+++ firstdaywork/PrintNames.java
@@ -0,0 +1,16 @@
+package first_package;
+
+public class PrintNames 
+{
+
+	public static void main(String args[])
+	{
+		System.out.println("Hi");
+		System.out.print(" Hello");
+		System.out.println("Raj");
+		
+
+	}//end of main
+}//end of class
+
+
